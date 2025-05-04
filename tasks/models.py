ea86a8137cf92393b62dbf2deb54c0f547c39bb5
basavaraj-ops <sagarbasu21@gmail.com>
2025-05-05 01:30:48 +0530

    first commit

--- tasks/models.py
+++ tasks/models.py
@@ -21,10 +21,4 @@ class Task(models.Model):
         return self.title
 
     
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
-    completed = models.BooleanField(default=False, blank=True, null=True)
 
-    def __str__(self):
-        return self.title
